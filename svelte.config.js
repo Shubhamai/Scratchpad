ca0834b9c2ea73bb41c54615e224c403637f0409
Shubhamai <shubham.aiengineer@gmail.com>
2024-04-08 16:30:14 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from "@sveltejs/adapter-auto";
-import adapter from "svelte-adapter-bun"; // NOTE: bun doesn't seems to work in vercel
+import adapter from "@sveltejs/adapter-auto";
+// import adapter from "svelte-adapter-bun"; // NOTE: bun doesn't seems to work in vercel
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import cspDirectives from './csp-directives.mjs';
